4d061b4f5a6120398f2b547c1585ee1d1316c587
lplnt <phd@live.ru>
2022-10-17 19:36:30 +0300

    Сделал 54 и 56 задание

---
+++ h_w_z54/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.Clear();
+Console.Write("Задайте количество строк в массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте количество столбцов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Получился массив:");
+
+var a = new int[m, n];
+var random = new Random();
+for (var i = 0; i < a.GetLength(0); i++, Console.WriteLine())
+{
+    for (var j = 0; j < a.GetLength(1); j++)
+    {
+        a[i, j] = random.Next(100);
+        Console.Write("{0,4}", a[i, j]);
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("Отсортированный массив: ");
+for (var i = 0; i < a.GetLength(0); i++)
+    for (var j = 0; j < a.GetLength(1); j++)
+        for (var k = 0; k < a.GetLength(1); k++)
+        {
+            if (a[i, j] <= a[i, k]) continue;
+            var temp = a[i, j];
+            a[i, j] = a[i, k];
+            a[i, k] = temp;
+        }
+
+for (var i = 0; i < a.GetLength(0); i++, Console.WriteLine())
+    for (var j = 0; j < a.GetLength(1); j++)
+    {
+        Console.Write("{0,4}", a[i, j]);
+    }
+
+
+int
+    max = a[0, 0],
+    min = a[0, 0],
+    maxI = 0,
+    maxJ = 0,
+    minI = 0,
+    minJ = 0;
+
+for (var i = 0; i < a.GetLength(0); i++)
+    for (var j = 0; j < a.GetLength(1); j++)
+    {
+        if (max < a[i, j])
+        {
+            max = a[i, j];
+            maxI = i;
+            maxJ = j;
+        }
+        if (min > a[i, j])
+        {
+            min = a[i, j];
+            minI = i;
+            minJ = j;
+        }
+    }
+
+
---
+++ h_w_z56/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу,
+// которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и 
+// выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+Console.Clear();
+Console.Write("Задайте количество столбцов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = n - 1;
+int sum1 = 0;
+int min1 = n * 10;
+int minI = 0;
+
+Console.WriteLine();
+Console.WriteLine("Получился массив:");
+int[,] a = new int[n, m];
+Random random = new Random();
+for (int i = 0; i < n; i++, Console.WriteLine())
+{
+    for (int j = 0; j < m; j++)
+    {
+        a[i, j] = random.Next(-10, 10);
+        Console.Write("{0,4}", a[i, j]);
+    }
+    Console.WriteLine();
+}
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+       sum1 += a[i, j];      
+    }
+    Console.WriteLine();
+    Console.WriteLine("Сумма " + (i + 1) + " строки равна " + sum1); 
+
+    if (min1 > sum1)
+       {
+       min1 = sum1;
+       minI = i + 1;
+       }
+    sum1 = 0;   
+}
+
+Console.WriteLine();
+Console.WriteLine($"Строка с минимальной суммой {minI}! Равная {min1}");
